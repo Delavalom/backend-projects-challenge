d6333845d88d9d1cd3d9824a1b60bed4f42a0880
Delavalom <laarvelo@icloud.com>
2023-01-26 19:53:34 -0400

    add the services and controller for auth, I have to add guards, serializer, module and quick test it out

--- nestjs-microservice/src/auth/controllers/auth.controller.ts
+++ nestjs-microservice/src/auth/controllers/auth.controller.ts
@@ -1,20 +1,22 @@
 import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 
-import { LocalGuard } from '../local.guard';
-import { AuthService } from './auth.service';
+// import { LocalGuard } from '../local.guard';
+import { AuthService } from '../services/auth.service';
+import { UserSchema } from 'src/users/entities/user.entity';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  registerUser(@Body() user: RegisterUserDto) {
+  registerUser(@Body() user: UserSchema) {
     return this.authService.registerUser(user);
   }
 
-  @UseGuards(LocalGuard)
+  // @UseGuards(LocalGuard)
   @Post('login')
-  loginUser(@Req() req, @Body() user: LoginUserDto) {
+  loginUser(@Req() req: Request, @Body() user: UserSchema) {
     return req.session;
   }
 }
--- nestjs-microservice/src/auth/services/auth.service.ts
+++ nestjs-microservice/src/auth/services/auth.service.ts
@@ -5,31 +5,15 @@ import { userSchema, UserSchema } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  private users: UserSchema[] = [
-    {
-      id: 1,
-      username: 'Joe',
-      email: 'joefoo@test.com',
-      // Passw0rd!
-      password: '$2b$12$s50omJrK/N3yCM6ynZYmNeen9WERDIVTncywePc75.Ul8.9PUk0LK',
-      role: 'admin',
-    },
-    {
-      id: 2,
-      username: 'Jen',
-      email: 'jenbar@test.com',
-      // P4ssword!
-      password: '$2b$12$FHUV7sHexgNoBbP8HsD4Su/CeiWbuX/JCo8l2nlY1yCo2LcR3SjmC',
-      role: 'user',
-    },
-  ];
+  private users = inMemoryUsers
 
   async validateUser(user: Pick<UserSchema, "email" | "password">) {
     const foundUser = this.users.find(u => u.email === user.email);
+    if (!foundUser) throw new UnauthorizedException()
     if (!user || !(await compare(user.password, foundUser?.password as string))) {
       throw new UnauthorizedException('Incorrect username or password');
     }
-    const { password: _password, ...retUser } = foundUser;
+    const { password, ...retUser } = foundUser;
     return retUser;
   }
 
@@ -56,10 +40,31 @@ export class AuthService {
   }
 
   findById(id: UserSchema["id"]): Omit<UserSchema, 'password'> {
-    const { password: _, ...user } = this.users.find(u => u.id === id);
+    const foundUser: UserSchema | undefined = this.users.find(u => u.id === id);
+    if (!foundUser) throw new UnauthorizedException()
+    const { password, ...user } = foundUser
     if (!user) {
       throw new BadRequestException(`No user found with id ${id}`);
     }
     return user;
   }
 }
+
+const inMemoryUsers: UserSchema[] = [
+  {
+    id: 1,
+    username: 'Joe',
+    email: 'joefoo@test.com',
+    // Passw0rd!
+    password: '$2b$12$s50omJrK/N3yCM6ynZYmNeen9WERDIVTncywePc75.Ul8.9PUk0LK',
+    role: 'admin',
+  },
+  {
+    id: 2,
+    username: 'Jen',
+    email: 'jenbar@test.com',
+    // P4ssword!
+    password: '$2b$12$FHUV7sHexgNoBbP8HsD4Su/CeiWbuX/JCo8l2nlY1yCo2LcR3SjmC',
+    role: 'user',
+  },
+];
